ce84d59cb48d09dfc6c961d104ad8525b467d087
Magomed Shamaev <ms77grz@gmail.com>
2020-12-31 03:30:34 +0300

    final commit

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ function generateList() {
     const div = document.createElement('div')
     const a = document.createElement('a')
     const p = document.createElement('p')
+    a.addEventListener('click', () => {
+      flyToStore(shop)
+    })
 
     div.classList.add('shop-item')
     a.innerText = shop.properties.name
@@ -27,3 +30,50 @@ function generateList() {
 }
 
 generateList()
+
+function makePopupContent(shop) {
+  return `
+    <div>
+      <h4>${shop.properties.name}</h4>
+      <p>${shop.properties.address}</p>
+      <div class="phone-number">
+        <a href="tel:${shop.properties.phone}">${shop.properties.phone}</a>
+      </div>
+    </div>
+  `
+}
+
+function onEachFeature(feature, layer) {
+  layer.bindPopup(makePopupContent(feature), {
+    closeButton: false,
+    offset: L.point(0, -8),
+  })
+}
+
+const markerIcon = L.icon({
+  iconUrl: './marker.png',
+  iconSize: [30, 40],
+})
+
+const shopsLayer = L.geoJSON(storeList, {
+  onEachFeature,
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, { icon: markerIcon })
+  },
+})
+
+shopsLayer.addTo(map)
+
+function flyToStore(store) {
+  const [lat, lng] = [...store.geometry.coordinates].reverse()
+  map.flyTo([lat, lng], 15, {
+    animate: false, // fast render
+    // duration: 3, // if animate: true
+  })
+  setTimeout(() => {
+    L.popup({ closeButton: false, offset: L.point(0, -8) })
+      .setLatLng([lat, lng])
+      .setContent(makePopupContent(store))
+      .openOn(map)
+  }, 1000) // 3000 if animate: true and duration: 3
+}
